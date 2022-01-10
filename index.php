884b602cb0483fa22ad171050a588496cefe0139
Van Payumo <payumo.van657@gmail.com>
2022-01-10 19:14:11 +0800

    removed commented code from index.php

--- index.php
+++ index.php
@@ -129,30 +129,6 @@ getPro();
         </div>
       </div>
 
-      <!-- <div class="banners">
-        <div class="container clearfix">
-
-          <div class="banner-award">
-            <span>Award winner</span><br> Fashion awards 2016
-          </div>
-
-          <div class="banner-social">
-            <a href="#" class="banner-social__link">
-            <i class="icon-facebook"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-twitter"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-instagram"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-pinterest-circled"></i>
-          </a>
-          </div>
-
-        </div>
-      </div> -->
 
       <div class="page-footer__subline">
         <div class="container clearfix">
