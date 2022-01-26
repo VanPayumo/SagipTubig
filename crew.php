868d1e35dc9df7e02049e2b3d7fce97c148ab387
acdeguia <67185278+acdeguia@users.noreply.github.com>
2022-01-26 11:19:49 +0800

    bati bati na sila

--- crew.php
+++ crew.php
@@ -7,8 +7,10 @@ include "includes/header.php";
 include "functions/functions.php";
 include "includes/main.php";
 
+
+
 ?>
 
-<div style = "padding: 0px; margin: 0px: ;" class="crew">
-<img  src="images/about.png" alt="aboutpage" width="1920px">
+<div class="crew">
+<img src="images/about.png" alt="aboutpage" width="1920px">
 </div>
